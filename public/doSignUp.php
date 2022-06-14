63d9cdf2be68fb6db01499f2cf178e9854648615
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 00:24:05 +0800

    Test code on server

--- public/doSignUp.php
+++ public/doSignUp.php
@@ -1,7 +1,14 @@
 <?php
     // Include config file
     //require_once "/db/auth/connect.php";
- 
+
+    echo "test";
+    echo $_POST["email"];
+    echo $_POST["password"];
+    echo $_POST["confirmPassword"];
+    echo $_POST["name"];
+    echo $_POST["telephone"];
+    echo $_POST["role"];
     // Define variables and initialize with empty values
     //$username = $password = $confirm_password = "";
     //$username_err = $password_err = $confirm_password_err = "";
@@ -9,7 +16,6 @@
     // Processing form data when form is submitted
     if($_SERVER["REQUEST_METHOD"] == "POST"){
  
-        echo "test";
         echo $_POST["email"];
         echo $_POST["password"];
         echo $_POST["confirmPassword"];
