ac6c81cfe0675a90b5d77df7f9d3b0e8cb4ff8d7
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 00:17:50 +0800

    Edit doSignUp.php

--- public/doSignUp.php
+++ public/doSignUp.php
@@ -1,22 +1,21 @@
 <?php
     // Include config file
-    require_once "/db/auth/connect.php";
+    //require_once "/db/auth/connect.php";
  
     // Define variables and initialize with empty values
-    $username = $password = $confirm_password = "";
-    $username_err = $password_err = $confirm_password_err = "";
-
-    echo $_POST["email"];
-    echo $_POST["password"];
-    echo $_POST["confirmPassword"];
-    echo $_POST["name"];
-    echo $_POST["telephone"];
-    echo $_POST["role"];
+    //$username = $password = $confirm_password = "";
+    //$username_err = $password_err = $confirm_password_err = "";
  
     // Processing form data when form is submitted
     if($_SERVER["REQUEST_METHOD"] == "POST"){
  
         echo "test";
+        echo $_POST["email"];
+        echo $_POST["password"];
+        echo $_POST["confirmPassword"];
+        echo $_POST["name"];
+        echo $_POST["telephone"];
+        echo $_POST["role"];
         
     }
 ?>
