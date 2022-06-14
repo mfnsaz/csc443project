22986f5a4bdd55b5efbb13bd421e32b97000c39c
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 00:04:34 +0800

    Redo PHP MySQL connection

---
+++ public/db/auth/connect.php
@@ -0,0 +1,20 @@
+<?php
+    $creds = parse_ini_file("/home/ubuntu/inis/dbcsc.ini");
+    $server = $creds["server"];
+    $username = $creds["username"];
+    $password = $creds["password"];
+    $database = $creds["database"];
+
+    /* Database credentials. Assuming you are running MySQL
+    server with imported settings */
+    define('DB_SERVER', $server);
+    define('DB_USERNAME', $username);
+    define('DB_PASSWORD', $password);
+    define('DB_NAME', $database);
+
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+    if (mysqli_connect_errno())
+    {
+        echo "Failed to connect to MySQL: ".mysqli_connect_error();
+    }
+?>
