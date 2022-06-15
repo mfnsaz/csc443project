e5ef7bd462e04ec88b8c753e40614f2b03238589
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-16 03:07:16 +0800

    Change header styling

--- public/admin/index.php
+++ public/admin/index.php
@@ -29,7 +29,9 @@
                 </div>
             </header>
         </div>
-        <h1>Welcome, <?php echo $_SESSION["name"] ?> to the admin panel</h1>
+        <div class="px-5 text-center">
+            <h1>Welcome, <?php echo $_SESSION["name"] ?> to the admin panel</h1>
+        </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
