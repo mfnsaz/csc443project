6265d143e5f6c2b1200faebf5e58a2a0b2a0b4ac
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 22:59:09 +0800

    Change to bootstrap 5

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>UiTM Club Activities Approval System</title>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     </head>
     <body>
         <div class="container">
