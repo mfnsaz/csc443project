9abc84f4ab7b73d7549d89a60bdad363c4dfc013
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 15:28:44 +0800

    Fix sign up validation

--- public/doSignUp.php
+++ public/doSignUp.php
@@ -20,27 +20,33 @@
                 $email = $_POST["email"];
                 if(empty(trim($_POST["email"]))){
                     $email_err = "Please enter an email.";
+                    die($email_err);
                 }
                 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     $emailErr = "Invalid email format";
+                    die($emailErr);
                 }
 
                 // Validate password
                 if(empty(trim($_POST["password"]))){
-                    $password_err = "Please enter a password.";     
+                    $password_err = "Please enter a password.";
+                    die($password_err);
                 } elseif(strlen(trim($_POST["password"])) < 8){
                     $password_err = "Password must have atleast 8 characters.";
+                    die($password_err);
                 } else{
                     $password = trim($_POST["password"]);
                 }
 
                 // Validate confirm password
                 if(empty(trim($_POST["confirm_password"]))){
-                    $confirm_password_err = "Please confirm password.";     
+                    $confirm_password_err = "Please confirm password.";
+                    die($confirm_password_err);
                 } else{
                     $confirm_password = trim($_POST["confirm_password"]);
                     if(empty($password_err) && ($password != $confirm_password)){
                         $confirm_password_err = "Password did not match.";
+                        die($confirm_password_err);
                     }
                 }
                 echo $_POST["name"];
