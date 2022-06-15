2b4d87f2ab54fb951c865cf85f5a87d283f342f9
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-15 15:57:38 +0800

    Try to fix bug

--- public/db/auth/connect.php
+++ public/db/auth/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $creds = parse_ini_file(__dir__."/.ini");
+    $creds = parse_ini_file("../../../.ini");
     $server = $creds["server"];
     $username = $creds["username"];
     $password = $creds["password"];
