b92027fcbdfd215617da062f266be36d1f85c03a
AzizahAzahar <103158469+AzizahAzahar@users.noreply.github.com>
2022-06-19 00:17:24 +0800

    commit

--- public/admin/index.php
+++ public/admin/index.php
@@ -8,7 +8,7 @@
 
 <!DOCTYPE html>
 <html>
-    <head>
+    <head>//title link
         <title>UiTM Club Activities Approval System</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     </head>
