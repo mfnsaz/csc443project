f53123d0c1bb4922b9d3b565937251a4d8f88363
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-22 00:21:54 +0800

    Downgrade code to allow usage of older php version

--- public/doSignUp.php
+++ public/doSignUp.php
@@ -15,7 +15,7 @@
 
             $backPage = $_SESSION["backPage"];
 
-            if(isset($_SESSION["uid"]) && !str_contains($backPage, "addNewUser.php")){
+            if(isset($_SESSION["uid"]) && strpos($backPage, 'addNewUser.php') === false){
                 //user is logged in already
                 $_SESSION["userErrCode"] = "SESSION_EXISTS";
                 $_SESSION["userErrMsg"] = "You are already logged in. Please log out to sign up as another user.";
