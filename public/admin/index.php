4e0bf3a92ba071cee8eae1cb03f5dfe45bcd8e0c
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-22 17:42:02 +0800

    Add view applications button

--- public/admin/index.php
+++ public/admin/index.php
@@ -22,6 +22,7 @@
         <div class="px-5">
             <h4 class="py-4">Available actions:</h4>
             <button type="button" class="btn btn-primary" onclick="location.href='/admin/addNewUser.php';">New User</button>
+            <button type="button" class="btn btn-primary" onclick="location.href='/admin/applicationList.php';">View Applications</button>
         </div>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
