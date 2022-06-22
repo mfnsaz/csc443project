c7c0d0e8ecdb58fd3f1fad3752828001ca29aa13
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-22 17:40:08 +0800

    Change code position

--- public/admin/applicationDetails.php
+++ public/admin/applicationDetails.php
@@ -11,33 +11,6 @@
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-        <script type="text/javascript">
-            var xmlhttp = new XMLHttpRequest();
-            var url = "/api/getOfficerList.php";
-            xmlhttp.onreadystatechange = function(){
-                if (this.readyState == 4 && this.status == 200) {
-                    var data = JSON.parse(this.responseText);
-                    var htmlData = "<option value=\"\"></option>";
-                    for(let i = 0; i < data.clubId.length; i++){
-                        htmlData = htmlData.concat("\n", "<option value=\""+data.clubId[i]+"\">"+data.clubName[i]+"</option>\n");
-                    }
-                    document.getElementById("officerList").innerHTML = htmlData;
-                }
-            }
-            xmlhttp.open("GET", url, true);
-            xmlhttp.send();
-
-            var roleSelection = document.getElementById('status');
-            roleSelection.onchange = function(){
-                if(roleSelection.selectedIndex === 1) {
-                    document.getElementById('offSelect').style.display = "block";
-                    document.getElementById('officerList').required = true;
-                } else {
-                    document.getElementById('offSelect').style.display = "none";
-                    document.getElementById('officerList').required = false;
-                }
-            }
-        </script>
         <?php
             include("../../header/header.php");
         ?>
@@ -97,5 +70,32 @@
 
                 </form>
           </div>
+          <script type="text/javascript">
+            var xmlhttp = new XMLHttpRequest();
+            var url = "/api/getOfficerList.php";
+            xmlhttp.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    var data = JSON.parse(this.responseText);
+                    var htmlData = "<option value=\"\"></option>";
+                    for(let i = 0; i < data.officerId.length; i++){
+                        htmlData = htmlData.concat("\n", "<option value=\""+data.officerId[i]+"\">"+data.officerName[i]+"</option>\n");
+                    }
+                    document.getElementById("officerList").innerHTML = htmlData;
+                }
+            }
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+
+            var roleSelection = document.getElementById('status');
+            roleSelection.onchange = function(){
+                if(roleSelection.selectedIndex === 1) {
+                    document.getElementById('offSelect').style.display = "block";
+                    document.getElementById('officerList').required = true;
+                } else {
+                    document.getElementById('offSelect').style.display = "none";
+                    document.getElementById('officerList').required = false;
+                }
+            }
+        </script>
     </body>
 </html>
