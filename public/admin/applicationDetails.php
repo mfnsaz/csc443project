44c23a6d46abf3bff3c89ac9b5dd8f6e1c22bf15
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-22 17:29:08 +0800

    Add code to dynamically change form

--- public/admin/applicationDetails.php
+++ public/admin/applicationDetails.php
@@ -7,6 +7,37 @@
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
     </head>
     <body>
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+        <script type="text/javascript">
+            var xmlhttp = new XMLHttpRequest();
+            var url = "/api/getOfficerList.php";
+            xmlhttp.onreadystatechange = function(){
+                if (this.readyState == 4 && this.status == 200) {
+                    var data = JSON.parse(this.responseText);
+                    var htmlData = "<option value=\"\"></option>";
+                    for(let i = 0; i < data.clubId.length; i++){
+                        htmlData = htmlData.concat("\n", "<option value=\""+data.clubId[i]+"\">"+data.clubName[i]+"</option>\n");
+                    }
+                    document.getElementById("officerList").innerHTML = htmlData;
+                }
+            }
+            xmlhttp.open("GET", url, true);
+            xmlhttp.send();
+
+            var roleSelection = document.getElementById('status');
+            roleSelection.onchange = function(){
+                if(roleSelection.selectedIndex === 1) {
+                    document.getElementById('offSelect').style.display = "block";
+                    document.getElementById('officerList').required = true;
+                } else {
+                    document.getElementById('offSelect').style.display = "none";
+                    document.getElementById('officerList').required = false;
+                }
+            }
+        </script>
         <?php
             include("../../header/header.php");
         ?>
@@ -40,6 +71,7 @@
                   </div>
                   <div class="form-floating mb-3">
                     <select class="form-select" name="appStatus" id="status" aria-label="appStat" required>
+                        <option value=""></option>
                         <option value="0">Accepted</option>
                         <option value="1">Rejected</option>
                     </select>
@@ -49,16 +81,8 @@
                   <input class="form-control" name="remarks" type="comment" placeholder="remarks" required/>
                   <label for="remarks">Remarks</label>
               </div>
-              <!--Aku try nak buat macam bila tekan accepted baru keluar list officer tapi tak jadi-->
-              <script>
-                var s = document.getElementById('status');
-                var status = s.options[s.selectedIndex].value;
-               if(status == '0'){
-                $("assignoff").show();
-              }
-               </script>
-                 <div class="form-floating mb-3">
-                    <select class="form-select" name="assignOfficer" id="assignoff" aria-label="assoff" required>
+                 <div class="form-floating mb-3" id="offSelect" style="display: none;">
+                    <select class="form-select" name="assignOfficer" id="officerList" aria-label="assoff" required>
                         <option value="0">Officer 1-Encik Nuh Hakimi Mohd Yassin</option>
                         <option value="1">Officer 2- Puan Mariya Mohamad</option>
                         <option value="2">Officer 3- Encik Fuad Ibrahim</option>
@@ -67,17 +91,11 @@
                 </div>
               
                 
-                  <div class="d-grid">
-                      <button class="btn btn-primary btn-lg" id="submitButton" type="submit">Submit</button>
-                  </div>
+                    <div class="d-grid">
+                        <button class="btn btn-primary btn-lg" id="submitButton" type="submit">Submit</button>
+                    </div>
 
-              </form>
+                </form>
           </div>
-</html>
-
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
     </body>
 </html>
