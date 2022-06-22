3b75d8542fd78d7e8aeb718b8a7ac88d345dc692
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-22 22:53:53 +0800

    Add tooltip thingy

--- public/admin/applicationList.php
+++ public/admin/applicationList.php
@@ -20,6 +20,8 @@
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
         <script type="text/javascript">
+            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
             $(document).ready( function () {
                 var mainTable = $('#appTable').DataTable({
                                     ajax: {
