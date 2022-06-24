0b63b247342cf50dcc0ba9fe488f5d4657681029
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-24 11:17:49 +0800

    Redo some code

--- header/footer.php
+++ header/footer.php
@@ -1,11 +1,8 @@
 <?php
     //footer code
     //get date and time
-    date_default_timezone_set("Asia/Kuala_Lumpur");
-    $dateNow = date('Y-m-d');
-    $timeNow = date('H:i:s');
-    $yearNow = date('Y');
+    require_once "../public/inc/globalVariables.php";
 ?>
 <div class="footer">
-    <b>©️ <?php $yearNow ?> MFNSAZ. UiTM Activities Application System</b>
+    <b>©️ <?php echo $yearNow; ?> MFNSAZ. UiTM Activities Application System</b>
 </div>
