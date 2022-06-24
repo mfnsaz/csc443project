188faf5e6b4d6b266e83eb304ac1fc95cd9df49e
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-24 11:10:36 +0800

    Change footer datetime code

--- header/footer.php
+++ header/footer.php
@@ -1,6 +1,10 @@
 <?php
     //footer code
-    require_once "../public/inc/globalVariables.php";
+    //get date and time
+    date_default_timezone_set("Asia/Kuala_Lumpur");
+    $dateNow = date('Y-m-d');
+    $timeNow = date('H:i:s');
+    $yearNow = date('Y');
 ?>
 <div class="footer">
     <b>©️ <?php $yearNow ?> MFNSAZ. UiTM Activities Application System</b>
