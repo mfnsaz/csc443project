571ea0d3999571fa76f7d04205110752885d5339
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-27 18:01:13 +0800

    Fix minor bug and clean code

--- public/api/getApplicationList.php
+++ public/api/getApplicationList.php
@@ -6,10 +6,10 @@
         if(isset($_GET["officer_id"])){
             $officerId = $_GET["officer_id"];
             $getAppSQL = "SELECT a.application_id, a.app_name, s.student_name, c.club_name, a.forwarded, a.approved FROM applications AS a JOIN students AS s ON a.student_id = s.student_id JOIN clubs AS c ON s.club_id = c.club_id WHERE a.officer_id = $officerId";
-        } else if(isset($_GET["student_id"])) {
+        } else if(isset($_GET["student_id"])){
             $studentId = $_GET["student_id"];
             $getAppSQL = "SELECT a.application_id, a.app_name, s.student_name, c.club_name, a.forwarded, a.approved FROM applications AS a JOIN students AS s ON a.student_id = s.student_id JOIN clubs AS c ON s.club_id = c.club_id WHERE a.student_id = $studentId";
-        } else if(isset($_GET["admin_id"])) {
+        } else if(isset($_GET["admin_id"])){
             $adminId = $_GET["admin_id"];
             $getAppSQL = "SELECT a.application_id, a.app_name, s.student_name, c.club_name, a.forwarded FROM applications AS a JOIN students AS s ON a.student_id = s.student_id JOIN clubs AS c ON s.club_id = c.club_id WHERE a.admin_id = $adminId";
         } else {
@@ -38,30 +38,34 @@
                     }
                 } else {
                     if($currApp[4] == 1){
-                        if(isset($_GET["officer_id"]) && $currApp[5] == NULL){
-                            array_push($outputRowData, "Reviewed, Pending Approval");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed">Approve Application</button>');
-                        } else if(isset($_GET["officer_id"]) && $currApp[5] == 0) {
-                            array_push($outputRowData, "Reviewed, Rejected");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Rejected</button>');
-                        } else if(isset($_GET["officer_id"]) && $currApp[5] == 1){
-                            array_push($outputRowData, "Reviewed, Approved");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Approved</button>');
-                        } else if(isset($_GET["student_id"]) && $currApp[5] == NULL){
-                            array_push($outputRowData, "Pending officer approval");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Approval Pending</button>');
-                        } else if(isset($_GET["student_id"]) && $currApp[5] == 0) {
-                            array_push($outputRowData, "Application rejected");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Rejected</button>');
-                        } else if(isset($_GET["student_id"]) && $currApp[5] == 1){
-                            array_push($outputRowData, "Application approved");
-                            array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Approved</button>');
+                        if(isset($_GET["officer_id"])){
+                            if($currApp[5] == NULL){
+                                array_push($outputRowData, "Reviewed, Pending Approval");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed">Approve Application</button>');
+                            } else if($currApp[5] == 1){
+                                array_push($outputRowData, "Reviewed, Approved");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Approved</button>');
+                            } else if($currApp[5] == 0){
+                                array_push($outputRowData, "Reviewed, Rejected");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Rejected</button>');
+                            }
+                        } else if(isset($_GET["student_id"])){
+                            if($currApp[5] == NULL){
+                                array_push($outputRowData, "Pending officer approval");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Approval Pending</button>');
+                            } else if($currApp[5] == 1){
+                                array_push($outputRowData, "Application rejected");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Rejected</button>');
+                            } else if($currApp[5] == 0){
+                                array_push($outputRowData, "Application approved");
+                                array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Approved</button>');
+                            }
                         } else {
                             array_push($outputRowData, "Reviewed, forwarded to Officer");
                             array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Reviewed</button>');
                         }
                     } else {
-                        if(isset("student_id")){
+                        if(isset($_GET["student_id"])){
                             array_push($outputRowData, "Application rejected");
                             array_push($outputRowData, '<button class="d-grid mx-auto btn btn-primary" style="display: block;" id="viewAppButton" data-bs-toggle="tooltip" data-bs-placement="top" title="Already Reviewed" disabled>Application Rejected</button>');
                         } else {
@@ -76,8 +80,8 @@
                 "data" => $outputTableData
             );
         } else {
-            echo mysqli_error($conn);
-            //header('X-PHP-Response-Code: 500', true, 500);
+            //echo mysqli_error($conn);
+            header('X-PHP-Response-Code: 500', true, 500);
             die();
         }
 
@@ -85,8 +89,8 @@
         echo json_encode($outputAppArr, JSON_PRETTY_PRINT);
         die();
     } else {
-        echo "did not use get method";
-        //header('X-PHP-Response-Code: 500', true, 500);
+        //echo "did not use get method";
+        header('X-PHP-Response-Code: 500', true, 500);
         die();
     }
 ?>
