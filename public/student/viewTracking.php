34c2af5efef49afe67c30cec3262daa06223c9b0
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-06-28 20:54:40 +0800

    Change studentId to appId

--- public/student/viewTracking.php
+++ public/student/viewTracking.php
@@ -4,7 +4,11 @@
         header("refresh:0;url=/login.php");
         die('<script>alert("STUDENT_ID NOT SET. INVALID SESSION.")</script>');
     }
-    $studentId = $_SESSION["student_id"];
+    if (!isset($_GET["app_id"])){
+        header("refresh:0;url=/officer/index.php");
+        die('<script>alert("APP_ID NOT SET. INVALID SESSION.")</script>');
+    }
+    $appId = $_GET["app_id"];
     $_SESSION["backPage"] = "applicationList.php";
 ?>
 <!DOCTYPE html>
@@ -15,8 +19,9 @@
         <title>UiTM Club Activities Approval System - Application List</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.min.css">
-        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.3.0/css/fixedHeader.dataTables.min.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.3.0/css/responsive.dataTables.css">
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/fixedheader/3.2.4/css/fixedHeader.bootstrap.css">
         <link rel="icon" type="image/x-icon" href="https://saringc19.uitm.edu.my/statics/icons/favicon.ico">
     </head>
     <body>
@@ -24,7 +29,7 @@
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.fixedHeader.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/fixedheader/3.2.4/js/fixedHeader.bootstrap.js"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.3.0/js/dataTables.responsive.min.js"></script>
         <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/responsive/2.3.0/js/responsive.bootstrap.min.js"></script>
         <script type="text/javascript">
@@ -33,29 +38,13 @@
             $(document).ready( function () {
                 var mainTable = $('#appTable').DataTable({
                                     ajax: {
-                                        url: '/api/getApplicationList.php?student_id=<?php echo $studentId ?>',
+                                        url: '/api/getApplicationList.php?app_id=<?php echo $appId ?>',
                                         responsive: true,
-                                        dataSrc: 'data',
-                                        columnDefs: [
-                                            {
-                                                targets: -1,
-                                                data: null,
-                                                defaultContent: '<button class="btn btn-primary btn-lg">View Application</button>',
-                                            },
-                                        ],
-                                    }
+                                        dataSrc: 'data'
+                                    },
+                                    fixedHeader: true
                                 });
-                $("#appTable tbody").on('click', 'button', function() {
-                    var data = mainTable.row($(this).parents('tr')).data();
-                    $("#viewAppButton").on('click', 'button', function() {
-                        window.location.href = "applicationDetails.php?app_id="+data[0];
-                    })
-                    $("#viewTrkButton").on('click', 'button', function() {
-                        window.location.href = "viewTracking.php?app_id="+data[0];
-                    })
-                })
-                new $.fn.dataTable.FixedHeader( table );
-            } );
+            });
         </script>
         <?php
             include("../../header/header.php");
