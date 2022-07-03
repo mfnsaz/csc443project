82b075d9277df05e134e9bcb88b95bcac70396e1
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-03 16:36:46 +0800

    Add code to debug breadcrumb

--- header/breadcrumb.php
+++ header/breadcrumb.php
@@ -12,6 +12,7 @@
                 $this->currtitle = $pageTitle;
             }
             $this->dirarray = explode("/",$this->currdir);
+            array_unshift($this->dirarray, "Home");
             array_push($this->disarray, $this->currtitle);
             foreach($this->dirarray as $crumb){
                 $this->crumbhtml = ucfirst(str_replace(array(".php","_"),array(""," "),$crumb) . ' ');
@@ -21,6 +22,15 @@
         function getFullCrumb(){
             return $this->crumbhtml;
         }
+
+        function debugVariables(){
+            $retArr = array();
+            array_push($retArr, print_r($this->currdir, true));
+            array_push($retArr, print_r($this->currtitle, true));
+            array_push($retArr, print_r($this->dirarray, true));
+            array_push($retArr, print_r($this->crumbhtml, true));
+            return $retArr;
+        }
     }
     if(isset($currDir) && isset($pageTitle)){
         $bcmb = new Breadcrumb($currDir, $pageTitle);
@@ -28,7 +38,7 @@
         echo $breadOut;
     } else {
         $bcmb = new Breadcrumb(__DIR__, "test");
-        print_r($bcmb->getFullCrumb());
+        print_r($bcmb->debugVariables());
         echo "Failed to get breadcrumb";
     }
 ?>
