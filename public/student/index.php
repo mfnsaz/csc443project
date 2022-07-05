87b3db0c0b00f8bc434fbf1081fc21beb58f4546
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-05 12:00:00 +0800

    Test design

--- public/student/index.php
+++ public/student/index.php
@@ -27,22 +27,24 @@
         <?php
             include("../../header/header.php");
         ?>
-        <div class="container">
-            <div class="row text-center">
+        <div class="px-5">
+            <div class="text-center">
                 <h1>Student Portal</h1>
             </div>
-            <div class="row py-3">
+            <div class="py-3">
                 <h4>Welcome, <?php echo $_SESSION["name"] ?> to the student portal</h4>
             </div>
-            <div class="row">
-                <h4 class="pb-4">Available actions:</h4>
-                <div class="col p-2">
-                    <b>Create new activity application: </b>
-                    <button type="button" class="btn btn-primary" onclick="location.href='/student/formApplication.php';">New Activity Application</button>
-                </div>
-                <div class="col p-2">
-                    <b>View activity applications list: </b>
-                    <button type="button" class="btn btn-primary" onclick="location.href='/student/applicationList.php';">View Application List</button>
+            <div class="container">
+                <div class="row">
+                    <h4 class="pb-4">Available actions:</h4>
+                    <div class="col p-2">
+                        <b>Create new activity application: </b>
+                        <button type="button" class="btn btn-primary" onclick="location.href='/student/formApplication.php';">New Activity Application</button>
+                    </div>
+                    <div class="col p-2">
+                        <b>View activity applications list: </b>
+                        <button type="button" class="btn btn-primary" onclick="location.href='/student/applicationList.php';">View Application List</button>
+                    </div>
                 </div>
             </div>
         </div>
