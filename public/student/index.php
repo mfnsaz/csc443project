d2da524455075f1bc439039d25572f50fbd5da1d
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-05 11:22:40 +0800

    Test responsive code

--- public/student/index.php
+++ public/student/index.php
@@ -17,6 +17,8 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>UiTM Club Activities Approval System - Student</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="icon" type="image/x-icon" href="https://saringc19.uitm.edu.my/statics/icons/favicon.ico">
