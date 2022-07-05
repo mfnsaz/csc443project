a91404c6b76a017d8514763563e4c55642ef1532
Megat Al Zhahir Daniel <megatalzhahir@gmail.com>
2022-07-05 17:22:12 +0800

    Change regex for php

--- header/breadcrumb.php
+++ header/breadcrumb.php
@@ -52,12 +52,12 @@
                 }
                 if(strpos($currPage, '.php')){
                     if($i == $iLimit){
-                        array_push($crumbArr, ucfirst(preg_replace("/^.*\.(php)$/im", $this->currtitle, $currPage)));
+                        array_push($crumbArr, ucfirst(preg_replace("/(?:(\S+.php|.php))/", $this->currtitle, $currPage)));
                     } else {
-                        array_push($crumbArr, ucfirst(preg_replace("/^.*\.(php)$/im", join(" ", preg_split('/(?=[A-Z])/',lcfirst($currPage), -1, PREG_SPLIT_NO_EMPTY)), $currPage)));
+                        array_push($crumbArr, ucfirst(preg_replace("/(?:(\S+.php|.php))/", join(" ", preg_split('/(?=[A-Z])/',lcfirst($currPage), -1, PREG_SPLIT_NO_EMPTY)), $currPage)));
                     }
                 } else {
-                    array_push($crumbArr, ucfirst(preg_replace("/^.*\.(php)$/im", join(" ", preg_split('/(?=[A-Z])/',lcfirst($currPage), -1, PREG_SPLIT_NO_EMPTY)), $currPage)));
+                    array_push($crumbArr, ucfirst(preg_replace("/(?:(\S+.php|.php))/", join(" ", preg_split('/(?=[A-Z])/',lcfirst($currPage), -1, PREG_SPLIT_NO_EMPTY)), $currPage)));
                 }
             }
             return $crumbArr;
